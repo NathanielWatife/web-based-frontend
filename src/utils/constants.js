c851033d7f335e190b3215a7e5493fcff555f4bb
NathanielWatife <awosanminathaniel0@gmail.com>
2025-10-14 13:15:07 +0100

    fix build

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const USER_ROLES = {
   STUDENT: 'student',
